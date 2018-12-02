a43c0a5ddea1eeb9a7e82540559fdd585b8d1e0e
Donald Cousar <drcousar@yahoo.com>
2018-12-02 13:17:33 -0600

    Add Assignment 1.4

---
+++ week-1/cousar-assignment-1.4.js
@@ -0,0 +1,34 @@
+// start program
+
+/*
+    Expected output:
+    
+    First name
+    Last name
+    Age
+    Street
+    City
+    State 
+    Zip code 
+*/ 
+
+// variable declaration and assignment goes here ...
+var firstName = "Donald";
+var lastName = "Cousar";
+var age = 42;
+var street = "123 Main Street";
+var city = "Jackson";
+var state = "Mississippi";
+var zipCode = "39211";
+
+// output 
+console.log('\n');
+console.log(firstName);
+console.log(lastName);
+console.log(age);
+console.log(street);
+console.log(city);
+console.log(state);
+console.log(zipCode);
+
+// end program 
