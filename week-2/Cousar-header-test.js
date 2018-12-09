ed75a4cfde2918b53c0630079cda9b6fd26b8166
Donald Cousar <drcousar@yahoo.com>
2018-12-09 16:00:02 -0600

    Add Assignment 2.2 Header

---
+++ week-2/Cousar-header-test.js
@@ -0,0 +1,14 @@
+/*
+============================================
+; Title:  header.js
+; Author: Professor Krasso 
+; Date:   9 Dec 2018
+; Modified By: Donald Cousar
+; Description: Displays a formatted header
+;===========================================
+*/
+//Call header code with functions
+const header = require('../Cousar-header.js');
+
+//Display header information using display function
+console.log(header.display("Donald", "Cousar","Week 2"));
