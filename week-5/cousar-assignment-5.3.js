f79362ac21046bc499b838a41490b3ca63454324
Donald Cousar <drcousar@yahoo.com>
2019-01-13 13:13:02 -0600

    Add Assignment 5.3

---
+++ week-5/cousar-assignment-5.3.js
@@ -0,0 +1,64 @@
+/*
+============================================
+; Title:  cousar-db.5.3.js
+; Author: Donald Cousar
+; Date:   13 Jan 2019
+; Description: Iterate over object using foreach 
+; Expected Output:
+
+  FirstName LastName
+  Exercise 5.3
+  Today's Date
+
+  -- COMPOSERS --
+  Last Name: Beethoven, Genre: Classical, Rating: 8
+  Last Name: Mozart, Genre: Classical, Rating: 10
+  Last Name: Bach, Genre: Classical, Rating: 9
+  Last Name: Haydn, Genre: Classical, Rating: 6
+  Last Name: Schubert, Genre: Classical, Rating: 5
+
+*/
+
+//Start Program
+var header = require('../cousar-header');
+console.log(header.display("Donald", "Cousar","Exersize 5.3"));
+console.log('\n');
+
+//Populate composers object
+var composers = [
+
+    {   
+        lastName: 'Beethoven',   
+        genre: 'Classical',  
+        rating: 8 
+    },
+    
+    {    
+        lastName: 'Mozart',   
+        genre: 'Classical',  
+        rating: 10  
+    },
+    
+    {  
+        lastName: 'Bach',   
+        genre: 'Classical',  
+        rating: 9
+    },
+    
+    {
+        lastName: 'Haydn',   
+        genre: 'Classical',  
+        rating: 6 
+    },
+    
+    {
+        lastName: 'Schubert',   
+        genre: 'Classical',  
+        rating: 5 
+    },
+    
+    ];
+    //iterate through object
+    composers.forEach(function(composer) {console.log("Last Name: " + composer.lastName + ", Genre: " + composer.genre + ", Rating: " + composer.rating)});
+
+//End Program
