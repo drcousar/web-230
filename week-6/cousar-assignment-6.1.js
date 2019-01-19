95f2e88e26d76324e64ebe4cf5fb136681881e00
Donald Cousar <drcousar@yahoo.com>
2019-01-19 15:26:23 -0600

    Add Assignment 6.2

---
+++ week-6/cousar-assignment-6.1.js
@@ -0,0 +1,25 @@
+/*
+============================================
+; Title:  cousar-assignment-6.1.js
+; Author: Donald Cousar
+; Date:   17 Jan 2019
+; Description: Debug my object properties
+;===========================================
+*/
+
+//BEGIN CODE
+
+// object
+const car = {
+    model: "Impala",
+    make: "Chevrolet",
+    year: "2004",
+    color: "white",
+    interior: "leather",
+    engine: 3.8
+  }
+  
+  // output
+  console.log('Your car is a ' + car.year + ' ' + car.model)
+  
+  //END CODE
---
+++ week-6/cousar-assignment-6.2.js
@@ -0,0 +1,29 @@
+/*
+============================================
+; Title:  cousar-db.6.2.js
+; Author: Donald Cousar
+; Date:   19 Jan 2019
+; Description: Try / catch error 
+; Expected Output:
+
+  FirstName LastName
+  Exercise 6.2
+  Today's Date
+
+  Catch clause: <your custom message goes here>
+  Finally clause reached...
+
+  */
+
+//Start Program
+var header = require('../cousar-header');
+console.log(header.display("Donald", "Cousar","Exersize 6.2"));
+console.log('\n');
+
+try {
+    eval("alert('This should catch an error)");
+} catch(err) {
+    console.log("Catch clasue: " + err);
+} finally {
+    console.log("Finally clause: End of program...");
+}
